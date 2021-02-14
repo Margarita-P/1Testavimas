fac5423701541f0d163a3d3abb9116f539ffc068
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-02-14 17:26:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,60 +6,67 @@ using std::endl;
 using std::cout;
 using std::cin;
 
-void skaiciavimas(string eilute, string &remelis1, string &remelis2, string &remelis3, string &remelis4)
+void skaiciavimas(string eilute, string& remelis, string& remelis1)
 {
     int ilgis = 0;
     ilgis = eilute.length();
     while (ilgis > 0)
     {
-        remelis1 += "*";
+        remelis += "*";
         ilgis--;
     }
     ilgis = eilute.length();
     while (ilgis > 0)
     {
-        remelis2 += " ";
+        remelis1 += " ";
         ilgis--;
         if (ilgis == 2)
-            remelis2 += "*"; 
+            remelis1 += "*"; 
     }
-    remelis3 = remelis2;
-    remelis4 = remelis1;
 }
-void Male(string vardas, string remelis1, string remelis2, string remelis3, string remelis4)
+void print(string remelis, string remelis1, int plotis, string eilute)
+{
+    int plotis1 = plotis;
+    cout << remelis << endl;
+    for (int i = 0; i < (plotis - 2) / 2; i++)
+        cout << remelis1 << endl;
+    cout << eilute << endl;
+    for (int i = 0; i < (plotis1 - 2)/ 2; i++)
+        cout << remelis1 << endl;
+    cout << remelis << endl;
+}
+void Male(string vardas, string remelis, string remelis1, int plotis)
 {
     string eilute1 = "* Sveikas, ";
     eilute1 += vardas + "! *";
-    skaiciavimas(eilute1, remelis1, remelis2, remelis3, remelis4);
-    cout << remelis1 << endl;
-    cout << remelis2 << endl;
-    cout << eilute1 << endl;
-    cout << remelis3 << endl;
-    cout << remelis4 << endl;
+    skaiciavimas(eilute1, remelis, remelis1);
+    print(remelis,remelis1,plotis,eilute1);
 }
-void Female(string vardas, string remelis1, string remelis2, string remelis3, string remelis4)
+void Female(string vardas, string remelis, string remelis1, int plotis)
 {
     string eilute2 = "* Sveika, ";
     eilute2 += vardas + "! *";
-    skaiciavimas(eilute2, remelis1, remelis2, remelis3, remelis4);
-    cout << remelis1 << endl;
-    cout << remelis2 << endl;
-    cout << eilute2 << endl;
-    cout << remelis3 << endl;
-    cout << remelis4 << endl;
+    skaiciavimas(eilute2, remelis, remelis1);
+    print(remelis, remelis1, plotis, eilute2);
 }
 int main()
 {
     string vardas;
-    string remelis1;
-    string remelis2 = "*";
-    string remelis3;
-    string remelis4;
+    string remelis;
+    string remelis1 = "*";
+    int plotis = 0;
     cout << "Iveskite savo varda\n";
     cin >> vardas;
+    cout << "iveskite remelio ploti" << endl;
+    cin >> plotis;    
+    if (plotis % 2 == 0 && plotis < 3)
+    {
+        cout << " ivedete neteisinga skaiciu" << endl;
+        return(0);
+    }    
     if (vardas.back() == 's')
-        Male(vardas, remelis1, remelis2, remelis3, remelis4);
+        Male(vardas, remelis, remelis1, plotis);
     else if (vardas.back() == 'a')
-        Female(vardas, remelis1, remelis2, remelis3, remelis4);
+        Female(vardas, remelis, remelis1, plotis);
     return(0);
 }
